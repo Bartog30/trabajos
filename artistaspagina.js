4727d6d30af4dc56c4066f4dcf222725216dd179
Bartog30 <115040852+Bartog30@users.noreply.github.com>
2022-10-04 17:05:13 -0300

    Add files via upload

---
+++ artistaspagina.js
@@ -0,0 +1,8 @@
+import React from "react";
+export default class artistaspagina extends React.Component {
+    render(){
+        return(
+            <p>Pagina de artistas</p>
+        );
+    }
+}
---
+++ contactopagina.js
@@ -0,0 +1,8 @@
+import React from "react";
+export default class contactopagina extends React.Component {
+    render(){
+        return(
+            <p>Pagina de contacto</p>
+        );
+    }
+}
